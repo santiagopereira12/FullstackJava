24a014ec5f5ca3f0d70e0798866a551b8aaa0684
santiagopereira12 <spereira98@misena.edu.co>
2025-04-21 19:39:45 -0500

    Inicio de Proyecto FullStack

---
+++ src/main/java/com/project/test/crudfullstack/CrudFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.project.test.crudfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudFullStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/test/crudfullstack/CrudFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.test.crudfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
